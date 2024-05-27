a05b1f1a54bb6dc8df620e916af7ce06dfead97b
yihshe <ys611@cam.ac.uk>
2024-05-27 20:11:10 +0000

    Mogi configs path updated

--- run.sh
+++ run.sh
@@ -5,8 +5,21 @@ export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 # python3 train.py --config configs/AE_RTM_A.json
 
 # Train AE_RTM_B (decoder being replaced with RTM)
-# python3 -m pdb train.py --config configs/AE_RTM_B.json
+# python3 train.py --config configs/AE_RTM_B.json
 
 # Train AE_RTM_C (encoder being replaced with RTM + correction layer)
-python3 train.py --config configs/AE_RTM_C.json
+# python3 train.py --config configs/AE_RTM_C.json
+
+# Train NN_RTM_D (baseline: neural network regressor)
+# python3 train.py --config configs/NN_RTM_D.json
+
+# Train AE_Mogi_A (classical autoencoder)
+# python3 train.py --config configs/AE_Mogi_A.json
+
+# Train AE_Mogi_B (decoder being replaced with Mogi)
+# python3 train.py --config configs/AE_Mogi_B.json
+
+# Train AE_Mogi_C (encoder being replaced with Mogi + correction layer)
+# python3 train.py --config configs/AE_Mogi_C.json
+
 
