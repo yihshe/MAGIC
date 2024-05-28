75f8505133177a5d9652ec95e43bc2288275307b
yihshe <ys611@cam.ac.uk>
2024-05-28 15:31:41 +0000

    floatchart updated

--- run_eval.sh
+++ run_eval.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 
-# # Evaluate AE_RTM_A (classical autoencoder)
+# Evaluate AE_RTM_A (classical autoencoder)
 python3 test_AE_RTM.py \
         --config pretrained/AE_RTM_A/config.json \
         --resume pretrained/AE_RTM_A/model_best.pth
 
-# # Evaluate AE_RTM_B (decoder being replaced with RTM)
+# Evaluate AE_RTM_B (decoder being replaced with RTM)
 python3 test_AE_RTM.py \
         --config pretrained/AE_RTM_B/config.json \
         --resume pretrained/AE_RTM_B/model_best.pth
@@ -21,17 +21,17 @@ python3 test_NN_RTM.py \
         --config pretrained/NN_RTM_D/config_infer_real.json \
         --resume pretrained/NN_RTM_D/model_best.pth
 
-# # Evaluate AE_Mogi_A (classical autoencoder)
+# Evaluate AE_Mogi_A (classical autoencoder)
 python3 test_AE_Mogi.py \
         --config pretrained/AE_Mogi_A/config.json \
         --resume pretrained/AE_Mogi_A/model_best.pth
 
-# # Evaluate AE_Mogi_B (decoder being replaced with Mogi)
+# Evaluate AE_Mogi_B (decoder being replaced with Mogi)
 python3 test_AE_Mogi.py \
         --config pretrained/AE_Mogi_B/config.json \
         --resume pretrained/AE_Mogi_B/model_best.pth
 
-# # Evaluate AE_Mogi_C (encoder being replaced with Mogi + correction layer)
+# Evaluate AE_Mogi_C (encoder being replaced with Mogi + correction layer)
 python3 test_AE_Mogi.py \
         --config pretrained/AE_Mogi_C/config.json \
         --resume pretrained/AE_Mogi_C/model_best.pth
